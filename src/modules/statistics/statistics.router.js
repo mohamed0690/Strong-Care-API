a59d71b06a78753a3d1d72eb14aa10229e805e7a
mohamed0690 <mohamedmahrous069@gmail.com>
2023-10-23 21:03:54 +0300

    add module statistics

--- src/modules/statistics/statistics.router.js
+++ src/modules/statistics/statistics.router.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { statisticsUsers } from './statistics.controller.js';
+import { authentication } from '../../../middleware/authentication.js';
+import { authorization } from '../../../middleware/authorization.js';
+import { Role } from '../../../enums/role.js';
 
 const statisticsRouter = express.Router();
 
 
-statisticsRouter.route('/').get(statisticsUsers);
+statisticsRouter.route('/').get(authentication, authorization(Role.ADMIN), statisticsUsers);
 
 export default statisticsRouter;

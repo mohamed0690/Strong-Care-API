29586b70cece7888e37bb4f834796435216443d8
mohamed0690 <mohamedmahrous069@gmail.com>
2023-10-24 17:05:28 +0300

    update statistics

--- database/statistics.js
+++ database/statistics.js
@@ -1,13 +1,15 @@
 import { catchAsyncError } from "../middleware/catchAsyncError.js";
+import { Compensations } from "./models/compensations.js";
 import { InsuranceRequest } from "./models/insurance.request.model.js";
 import { User } from "./models/user.model.js";
+import { Order } from './models/order.model.js';
 
 
 export const statistics = catchAsyncError(async () => {
-    const pipeline = [
+    const pipeline = (column) => [
         {
             $group: {
-                _id: "$role",
+                _id: `$${column}`,
                 count: { $sum: 1 },
             },
         },
@@ -16,27 +18,25 @@ export const statistics = catchAsyncError(async () => {
         },
     ];
 
-    const pipeline2 = [
-        {
-            $group: {
-                _id: "$state",
-                count: { $sum: 1 },
-            },
-        },
-        {
-            $sort: { _id: 1 },
-        },
-    ];
 
-    const result = await User.aggregate(pipeline).exec();
-    const result2 = await InsuranceRequest.aggregate(pipeline).exec();
+
+    const result = await User.aggregate(pipeline("role")).exec();
+    const result2 = await InsuranceRequest.aggregate(pipeline("state")).exec();
+    const result3 = await Compensations.aggregate(pipeline()).exec();
+    const result4 = await Order.aggregate(pipeline("isPaid")).exec();
 
     const userStatistics = {};
     result.forEach((entry) => {
         userStatistics[entry._id] = entry.count;
     });
     result2.forEach((entry) => {
-        userStatistics[`InsuranceRequest${entry._id}`] = entry.count;
+        userStatistics[`InsuranceRequest_${entry._id}`] = entry.count;
+    });
+    result3.forEach((entry) => {
+        userStatistics[`Compensations`] = entry.count;
+    });
+    result4.forEach((entry) => {
+        userStatistics[`orders_${entry._id}`] = entry.count;
     });
 
     return userStatistics;
